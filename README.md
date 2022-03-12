df77d7f174906d851564cd274db6ff3f7bf8d6fd
Canaan <cjmckenz@uvm.edu>
2022-03-12 13:11:41 -0500

    TODO add security layer between two nodes?

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ TO DO:
 
 + consensus between nodes to prevent simultaneous node creation aka no two nodes #4 pointing to #3 for example, create "retry mechanism"
 	- rudimentary consensus determines longest "most up to date chain from either local and remote and uses that"
+	
++ security layer between connecting nodes, currently client request and node response are broadcast through the entire network
--- src/p2p.rs
+++ src/p2p.rs
@@ -4,17 +4,18 @@ use super::{App, Block};
 use log::{error,info};
 use serde::{Serialize,Deserialize};
 use libp2p::{
-	floodsub::{Floodsub,FloodsubEvent,Topic},
-	identity,
+	floodsub::{Floodsub,FloodsubEvent,Topic}, //libp2p's publish subscribe protocol
+	identity, //identify client in network
 	mdns::{Mdns,MdnsEvent},
 	swarm::{NetworkBehaviorEventProcess,Swarm},
-	NetworkBehavior,PeerId,
+	NetworkBehavior,
+	PeerId,
 };
 
 
 pub static KEYS: Lazy = Lazy::new(identity::Keypair::generate_ed25519);
 pub static PEER_ID: Lazy =Lazy::new( || PeerId::from(KEYS.public()));
-pub static CHAIN_TOPIC: Lazy = Lazy::new( || Topic::new("chains"));
+pub static CHAIN_TOPIC: Lazy = Lazy::new( || Topic::new("chains")); //simple - broadcasts to all nodes on network client request and our response, this is ipfs but needs security layer
 pub static BLOCK_TOPIC: Lazy = Lazy::new( || Topic::new("blocks"));
 
 #[derive(Serialize,Deserialize,Debug)]
@@ -45,3 +46,4 @@ pub struct AppBehavior {
 	#[behavior(ignore)]
 	pub app: App,
 }
+
