54e963254d941f00e075329e1c2fd1ca777eed3a
Canaan <cjmckenz@uvm.edu>
2022-03-12 14:03:38 -0500

    add libp2p mdns-floodsub handlers for NetworkBehaviorEvent

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ TO DO:
 
 + consensus between nodes to prevent simultaneous node creation aka no two nodes #4 pointing to #3 for example, create "retry mechanism"
 	- rudimentary consensus determines longest "most up to date chain from either local and remote and uses that"
-	
+
 + security layer between connecting nodes, currently client request and node response are broadcast through the entire network
+	- update with libp2p point-to-point request/response model to improve performance...see libp2p docs request_response
+	- refactor with gossipsub for more efficiency
--- src/p2p.rs
+++ src/p2p.rs
@@ -29,6 +29,7 @@ pub struct LocalChainRequest {
 	pub from_peer_id: String,
 }
 
+//keep application in sync with incoming and outgoing network traffic
 pub enum EventType{
 	LocalChainRequest(ChainResponse),
 	Input(String),
@@ -45,5 +46,52 @@ pub struct AppBehavior {
 	pub init_sender: mpsc::UnboundSender,
 	#[behavior(ignore)]
 	pub app: App,
+}
+
+impl AppBehavior {
+	pub async fn new(
+		app: App,
+		response_sender: mpsc::UnboundSender,
+		init_sender: mpsc::UnboundSender,
+	) -> Self {
+		let mut behavior = Self {
+			app,
+			floodsub: Floodsub::new(*PEER_ID),
+			mdns: Mdns::new(Default::default())
+			.await
+			.expect("can create mdns"),
+			response_sender,
+			init_sender,
+		};
+		behavior.floodsub.subscibe(CHAIN_TOPIC.clone());
+		behavior.floodsub.subscibe(BLOCK_TOPIC.clone());
+
+		behavior
+	}
+}
+
+//use multicast dns handler from libp2p
+impl NetworkBehaviorEventProcess<MdnsEvent> for AppBehavior {
+	fn inject_event(&mut self, event: MdnsEvent){
+		match event {
+			MdnsEvent::Discovered(discovered_list) => {
+				for (peer, _addr) in discovered_list {
+					self.floodsub.add_node_to_partial_view(peer);
+				}
+			}
+			MdnsEvent::Expired(expired_list) => {
+				for (peer, _addr) in expired_list {
+					if !self.mdns.has_node(&peer) {
+						self.floodsub.remove_node_from_partial_view(&peer);
+					}
+				}
+			}
+		}
+	}
+}
+
+//incoming event handler
+imp NetworkBehaviorEventProcess for AppBehavior {
+	
 }
 
