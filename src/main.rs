5b79a0d6e3da416ebb90ffbef193425472734a70
Canaan <cjmckenz@uvm.edu>
2022-03-12 09:15:15 -0500

    add genesis block

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Serialize,Deserialize};
-
+use chrono::prelude::*;
 pub struct App {
 
     pub blocks: Vec<Block>,
@@ -10,7 +10,7 @@ pub struct Block {
     pub id: u64,
     pub hash: String,
     pub previous_hash: String,
-    pub timestamp: u64,
+    pub timestamp: i64,
     pub data: String,
     pub nonce: u64,
 }
@@ -18,6 +18,18 @@ pub struct Block {
 impl App {
     fn new() -> Self{
         Self {blocks: vec![]}
+    }
+
+    fn genesis(&mut self){
+        let genesis_block =  Block{
+            id: 0,
+            timestamp: Utc::now().timestamp(),
+            previous_hash: String::from("genesis"),
+            data: String::from("genesis!"),
+            nonce: 2836,
+            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
+        };
+        self.blocks.push(genesis_block);
     }
 }
 
