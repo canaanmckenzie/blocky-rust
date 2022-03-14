09289d504834ef90fdbdc2eee59f224f204b7e87
Canaan <cjmckenz@uvm.edu>
2022-03-14 11:17:51 -0400

    add handler functions for printing and creating new block

--- src/p2p.rs
+++ src/p2p.rs
@@ -131,5 +131,41 @@ impl NetworkBehaviourEventProcess<FloodsubEvent> for AppBehaviour {
 //helper functions for user input
 
 pub fn get_list_peers(swarm: &Swarm<AppBehaviour>) -> Vec<String>{
-	
+	info!("Discovered Peers:");
+	let nodes = swarm.behaviour().mdns.discovered_nodes();
+	let mut unique_peers = HashSet::new();
+	for peer in nodes{
+		unique_peers.insert(peer);
+	}
+	unique_peers.iter().map(|p| p.to_string()).collect()
 } 
+
+
+pub fn handle_print_peers(swarm: &Swarm<AppBehaviour>) {
+	let peers = get_list_peers(swarm);
+	peers.iter().for_each(|p| info!("{}",p));
+}
+
+pub fn handle_print_chain(swarm: &Swarm<AppBehaviour>){
+	info!("Local Blockchain:");
+	let pretty_json = 
+		serde_json::to_string_pretty(&swarm.behaviour().app.blocks().expect("can jsonify blocks"));
+	info!("{}", pretty_json);
+}
+
+pub fn handle_create_block(cmd: &str, swarm &mut Swarm<AppBehaviour>){
+	if let Some(data) = cmd.strip_prefix("create b"){
+		let behaviour = swarm.behaviour_mut();
+		let latest_block = behaviour.app.blocks.last().expect("there is at least one block");
+		let block = Block::new(){
+			latest_block.id+1,
+			latest_block.hash.clone(),
+			data.to_owned(),
+		};
+
+		let json = serde_json::to_string(&block).expect("can jsonify request");
+		behaviour.app.blocks.push(block);
+		info!("Broadcasting new block");
+		behaviour.floodsub.publish(BLOCK_TOPIC.clone(),json.as_byes());
+	}
+}
